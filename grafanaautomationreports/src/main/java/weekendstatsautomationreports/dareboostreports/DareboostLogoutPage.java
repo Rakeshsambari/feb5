4ab5ead1e2a80755747fcf8d26f1ccd1380f3ae2
RAKESH <perfengineerrakesh@gmail.com>
2020-02-05 23:31:24 +0530

    firstcommit

---
+++ grafanaautomationreports/src/main/java/weekendstatsautomationreports/dareboostreports/DareboostLogoutPage.java
@@ -0,0 +1,10 @@
+package weekendstatsautomationreports.dareboostreports;
+
+public class DareboostLogoutPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
